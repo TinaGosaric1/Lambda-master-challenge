2643ac1c7ca5cf802e35af35a3684ceb0eeb7ff0
Tina Gosarič <tina.gosaric@gorenje.com>
2022-11-30 10:39:25 +0100

    Assigning supplier to a variable

--- src/com/tg/Main.java
+++ src/com/tg/Main.java
@@ -5,22 +5,8 @@ import java.util.function.Supplier;
 
 public class Main {
     public static void main(String[] args) {
-
-        Function<String /*parameter*/, String /*return value*/> lambdaFunction = s -> {
-            StringBuilder returnVal = new StringBuilder();
-            for (int i = 0; i < s.length(); i++) {
-                if (i % 2 == 1) {
-                    returnVal.append(s.charAt(i));
-                }
-            }
-            return returnVal.toString();
-        };
-        System.out.println(everySecondCharacter(lambdaFunction, "1234567890"));
-
         Supplier<String> supplier = () -> "Today is Monday"; // Supplier produces objects
-    }
-
-    public static String everySecondCharacter(Function<String, String> func, String s1) {
-        return func.apply(s1);
+        String supplierResult = supplier.get();
+        System.out.println(supplierResult);
     }
 }
