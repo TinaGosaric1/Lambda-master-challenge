24c3d392cc21c98a04de22398bd5b5260c0c2edb
Tina Gosarič <tina.gosaric@gorenje.com>
2022-11-30 10:13:37 +0100

    Passing and executing lambdaFunction in a method

--- src/com/tg/Main.java
+++ src/com/tg/Main.java
@@ -1,12 +1,10 @@
 package com.tg;
 
-import java.util.concurrent.Flow;
 import java.util.function.Function;
 
 public class Main {
     public static void main(String[] args) {
 
-        //with lambda
         Function<String /*parameter*/, String /*return value*/> lambdaFunction = s -> {
             StringBuilder returnVal = new StringBuilder();
             for (int i = 0; i < s.length(); i++) {
@@ -16,18 +14,10 @@ public class Main {
             }
             return returnVal.toString();
         };
-
-        System.out.println(lambdaFunction.apply("1234567890"));
+        System.out.println(everySecondCharacter(lambdaFunction, "1234567890"));
     }
 
-    //without lambda
-    public static String everySecondChar(String source) {
-        StringBuilder returnVal = new StringBuilder();
-        for (int i = 0; i < source.length(); i++) {
-            if (i % 2 == 1) {
-                returnVal.append(source.charAt(i));
-            }
-        }
-        return returnVal.toString();
+    public static String everySecondCharacter(Function<String, String> func, String s1) {
+        return func.apply(s1);
     }
 }
