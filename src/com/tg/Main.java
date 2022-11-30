82784b43b0f5e0c14bf6386010df6c4c3f6db170
Tina Gosarič <tina.gosaric@gorenje.com>
2022-11-30 10:00:16 +0100

    Adding comment regarding functional interface

--- src/com/tg/Main.java
+++ src/com/tg/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
 
         //with lambda
-        Function<String, String> lambdaFunction = s -> {
+        Function<String /*parameter*/, String /*return value*/> lambdaFunction = s -> {
             StringBuilder returnVal = new StringBuilder();
             for (int i = 0; i < s.length(); i++) {
                 if (i % 2 == 1) {
@@ -18,7 +18,6 @@ public class Main {
         };
 
         System.out.println(lambdaFunction.apply("1234567890"));
-        System.out.println(everySecondChar("1234567890"));
     }
 
     //without lambda
